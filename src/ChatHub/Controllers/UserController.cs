bbd9c0e0f6eb5f366077d628366a11ec70e1a23b
Salman <35454621+SalmanBasmechi@users.noreply.github.com>
2019-02-23 08:18:03 +0330

    fix login data access

--- src/ChatHub/Controllers/UserController.cs
+++ src/ChatHub/Controllers/UserController.cs
@@ -50,7 +50,7 @@ namespace ChatHub.Controllers
         [HttpPost, AllowAnonymous]
         public async Task<IActionResult> Login([FromBody] string mobile)
         {
-            User user = await dbContext.Users.SingleOrDefaultAsync(c => c.Mobile == mobile);
+            User user = await dbContext.Users.FirstOrDefaultAsync(c => c.Mobile == mobile);
 
             if (user == null)
             {
